ba1262248e17d366cc72ea6cc0c238b351c2808e
max-donger <maxwitte1991@gmail.com>
2020-06-14 17:53:18 +0200

    Initial draft for fetching website content

---
+++ Crawler.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace RedditScanner
+{
+    public class PostInfo
+    {
+        public string Title { get; set; }
+        public int UpvoteCount { get; set; }
+        public string Url { get; set; }
+    }
+    public class Crawler
+    {
+        private HttpClient client = new HttpClient();
+        public async Task<IEnumerable<PostInfo>> GetLatestPostsFromSubreddit(string subreddit)
+        {
+            var jsonStr = await client.GetStringAsync(string.IsNullOrEmpty(subreddit) ? $"https://reddit.com/.json" : $"https://reddit.com/r/{subreddit}.json");
+
+            var subredditResponse = JsonConvert.DeserializeObject<dynamic>(jsonStr);
+
+            // Only this and the jsonStr is probably needed if im gonna write to a file, and i should return from that file instead?
+            System.IO.File.WriteAllText("G:/Electron/source/repos/rackham-realestatecrawler/out/real-estate.txt", jsonStr);
+
+            return ((IEnumerable<dynamic>)subredditResponse.data.children).Select(post => new PostInfo
+            {
+                Title = post.data.title,
+                UpvoteCount = post.data.ups,
+                Url = post.data.url
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,64 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
 using ElectronCgi.DotNet;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
 
-namespace Core
+
+namespace RedditScanner
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var redditClient = new Crawler();
             var connection = new ConnectionBuilder()
-                .WithLogging(minimumLogLevel: LogLevel.Trace, logFilePath: "rackham-trace.log")
-                .Build();
-            
-            // expects a request named "greeting" with a string argument and returns a string
-            connection.On<string, string>("greeting", name => "Hello " + name);
-            connection.Listen();    
+                                    .WithLogging(minimumLogLevel: LogLevel.Trace, logFilePath: "rackham-trace.log")
+                                    .Build();
+
+            var selectedSubreddit = "";
+            connection.On("select-subreddit", (string newSubreddit) =>
+            {
+                selectedSubreddit = newSubreddit;
+            });
+
+            CancellationTokenSource cancelationTokenSource = new CancellationTokenSource();
+            connection.OnAsync("start", async () =>
+            {
+                Console.Error.WriteLine("started"); //this will show up in the node console
+                await Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        if (cancelationTokenSource.Token.IsCancellationRequested)
+                            return;
+                        try
+                        {
+                            var posts = await redditClient.GetLatestPostsFromSubreddit(selectedSubreddit);
+                            connection.Send("show-posts", posts);
+                            await Task.Delay(5000);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Error.WriteLine($"Failed to get posts for {selectedSubreddit} (maybe it does not exist?)"); //this will show up in the node console
+                            Console.Error.WriteLine(ex.Message);
+                            return;                            
+                        }
+                    }
+                }, cancelationTokenSource.Token);
+            });
+
+            connection.On("stop", () =>
+            {
+                Console.Error.WriteLine("Stop");
+                if (cancelationTokenSource != null)
+                {
+                    cancelationTokenSource.Cancel(); //this will cause the start request to complete
+                }
+            });
+
+            connection.Listen();            
         }
     }
 }
