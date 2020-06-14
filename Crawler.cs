aba269e7e012d459d66ef28bc238cf7f8107860e
max-donger <maxwitte1991@gmail.com>
2020-06-15 00:03:43 +0200

    Add async for crawler and split files

--- Crawler.cs
+++ Crawler.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Linq;
 
-namespace RedditScanner
+namespace Rackham
 {
     public class PostInfo
     {
@@ -15,21 +15,15 @@ namespace RedditScanner
     public class Crawler
     {
         private HttpClient client = new HttpClient();
-        public async Task<IEnumerable<PostInfo>> GetLatestPostsFromSubreddit(string subreddit)
+
+        public async Task Start(string subreddit)
         {
             var jsonStr = await client.GetStringAsync(string.IsNullOrEmpty(subreddit) ? $"https://reddit.com/.json" : $"https://reddit.com/r/{subreddit}.json");
 
             var subredditResponse = JsonConvert.DeserializeObject<dynamic>(jsonStr);
 
             // Only this and the jsonStr is probably needed if im gonna write to a file, and i should return from that file instead?
-            System.IO.File.WriteAllText("G:/Electron/source/repos/rackham-realestatecrawler/out/real-estate.txt", jsonStr);
-
-            return ((IEnumerable<dynamic>)subredditResponse.data.children).Select(post => new PostInfo
-            {
-                Title = post.data.title,
-                UpvoteCount = post.data.ups,
-                Url = post.data.url
-            });
+            System.IO.File.WriteAllText("G:/Electron/source/repos/rackham-realestatecrawler/out/real-estate.txt", jsonStr);            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,60 +5,20 @@ using System.Threading.Tasks;
 using System.Threading;
 using Newtonsoft.Json;
 
-
-namespace RedditScanner
+namespace Rackham
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var redditClient = new Crawler();
-            var connection = new ConnectionBuilder()
-                                    .WithLogging(minimumLogLevel: LogLevel.Trace, logFilePath: "rackham-trace.log")
-                                    .Build();
+            var service = new APIService();
+            var crawler = new Crawler();
+            var selectedSubreddit = "TibiaMMO";
 
-            var selectedSubreddit = "";
-            connection.On("select-subreddit", (string newSubreddit) =>
-            {
-                selectedSubreddit = newSubreddit;
-            });
+            await crawler.Start(selectedSubreddit);
 
-            CancellationTokenSource cancelationTokenSource = new CancellationTokenSource();
-            connection.OnAsync("start", async () =>
-            {
-                Console.Error.WriteLine("started"); //this will show up in the node console
-                await Task.Run(async () =>
-                {
-                    while (true)
-                    {
-                        if (cancelationTokenSource.Token.IsCancellationRequested)
-                            return;
-                        try
-                        {
-                            var posts = await redditClient.GetLatestPostsFromSubreddit(selectedSubreddit);
-                            connection.Send("show-posts", posts);
-                            await Task.Delay(5000);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.Error.WriteLine($"Failed to get posts for {selectedSubreddit} (maybe it does not exist?)"); //this will show up in the node console
-                            Console.Error.WriteLine(ex.Message);
-                            return;                            
-                        }
-                    }
-                }, cancelationTokenSource.Token);
-            });
-
-            connection.On("stop", () =>
-            {
-                Console.Error.WriteLine("Stop");
-                if (cancelationTokenSource != null)
-                {
-                    cancelationTokenSource.Cancel(); //this will cause the start request to complete
-                }
-            });
-
-            connection.Listen();            
+            // Start the API Service. Must be run last because it is not async.
+            APIService.Start();
         }
     }
 }
